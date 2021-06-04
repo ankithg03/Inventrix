7fc207db1f3c54ec71efccc14d63231785aa55a7
Usha-hb <usha.h@codilar.com>
2021-06-04 18:05:05 +0530

    hello word module added

---
+++ code/Codilar/HomePage/Controller/Index/Index.php
@@ -0,0 +1,44 @@
+<?php
+
+
+namespace Codilar\HomePage\Controller\Index;
+
+
+use Magento\Customer\Model\Session;
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\App\ResponseInterface;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index extends Action
+{
+    /**
+     * @var PageFactory
+     */
+    private $pageFactory;
+
+
+    /**
+     * Index constructor.
+     * @param Context $context
+     * @param PageFactory $pageFactory
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $pageFactory
+    ) {
+        parent::__construct($context);
+        $this->pageFactory = $pageFactory;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function execute()
+    {
+        
+        return $this->pageFactory->create();
+
+    }
+}
+
---
+++ code/Codilar/HomePage/registration.php
@@ -0,0 +1,6 @@
+<?php
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Codilar_HomePage', __DIR__);
+
